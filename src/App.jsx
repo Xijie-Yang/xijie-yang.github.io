31005d58386e969005b1f64cfb6293cf3261f639
YangXijie <564197835@qq.com>
2026-04-27 14:16:58 +0800

    add initial contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,167 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-     <h1>Xijie Yang Researcher Homepage</h1>
+      <h1>Xijie Yang | Researcher Homepage</h1>
+
+      {/* WIP a brief introduction */}
+
+      <h2>About Me</h2>
+
+      <p>
+        <span title="杨希杰">Xijie Yang</span>
+      </p>
+
+      <p>(2023-present)</p>
+      <p>
+        Ph.D. candidate in{" "}
+        <a
+          href="http://www.en.cs.zju.edu.cn"
+          title="浙江大学计算机科学与技术学院"
+        >
+          CS @ Zhejiang University
+        </a>{" "}
+        (ZJU) | Intern in{" "}
+        <a
+          href="https://www.shlab.org.cn"
+          title="上海人工智能实验室具身智能中心"
+        >
+          Embodied AI Center @ Shanghai Artificial Intelligence Laboratory
+        </a>{" "}
+        (SH AILAB)
+      </p>
+      <p>
+        Mentored by{" "}
+        <a href="https://eveneveno.github.io/lnxu/" title="徐霖宁">
+          Linning Xu
+        </a>{" "}
+        and{" "}
+        <a href="https://mulinyu.github.io" title="于沐霖">
+          Mulin Yu
+        </a>
+        ; Supervised by{" "}
+        <a href="https://datascience.hku.hk/people/bo-dai/" title="戴勃">
+          Prof. Bo Dai
+        </a>{" "}
+        and{" "}
+        <a href="https://www.ie.cuhk.edu.hk/faculty/lin-dahua/" title="林达华">
+          Prof. Dahua Lin
+        </a>
+        .
+      </p>
+      <p>
+        Research Interests: 3D Reconstruction, Differentiable Rendering, 3D
+        Representation, Interaction
+      </p>
+      <p>(2019-2023)</p>
+      <p>
+        B.S. in{" "}
+        <a href="https://www.ee.tsinghua.edu.cn/en/" title="清华大学电子工程系">
+          EE @ Tsinghua University
+        </a>{" "}
+        (THU)
+      </p>
+
+      <h2>Publications</h2>
+
+      {/* WIP add authors, teaser, abs / brief intro */}
+      <p>
+        V3DG (SIGGRAPH 2025):{" "}
+        <a href="https://xijie-yang.github.io/V3DG/">Project Page</a>,{" "}
+        <a href="https://arxiv.org/abs/2505.06523">Paper (arXiv)</a>,{" "}
+        <a href="https://doi.org/10.1145/3721238.3730602">Paper (Publisher)</a>,{" "}
+        <a href="https://github.com/city-super/V3DG">Code</a>
+      </p>
+      <p>
+        EAG-PT (SIGGRAPH 2026):{" "}
+        <a href="https://eag-pt.github.io">[WIP] Project Page</a>,{" "}
+        <a href="https://arxiv.org/abs/2601.23065">Paper (arXiv)</a>,{" "}
+        <a href="https://doi.org/10.1145/3799902.3811054">
+          [WIP] Paper (Publisher)
+        </a>
+        , <a href="https://github.com/InternRobotics/EAG-PT">[WIP] Code</a>
+      </p>
+
+      <h2>Contact Me</h2>
+
+      <p>
+        Personal Email: <a href="mailto:564197835@qq.com">564197835@qq.com</a>
+      </p>
+      <p>
+        Affliation Emails:{" "}
+        <a href="mailto:yangxijie@pjlab.org.cn">yangxijie@pjlab.org.cn</a>,{" "}
+        <a href="mailto:yangxijie@zju.edu.cn">yangxijie@zju.edu.cn</a>
+      </p>
+      <p>WeChat: y564197835</p>
+
+      <h2>Links</h2>
+
+      <p></p>
+      <p>
+        <a href="https://orcid.org/0009-0009-3076-2595">ORCID</a>,{" "}
+        <a href="https://github.com/Yang-Xijie">GitHub/Yang-Xijie</a>,{" "}
+        <a href="https://github.com/Xijie-Yang">GitHub/Xijie-Yang</a>,{" "}
+        <a href="https://xijie-yang.github.io/SDG/">SDG Publications</a>
+      </p>
+
+      <p>
+        <a href="https://www.xiaohongshu.com/user/profile/61be0cc10000000021024369">
+          RedNote
+        </a>{" "}
+        (Chinese Social Media),{" "}
+        <a href="https://space.bilibili.com/24502827">Bilibili</a> (Chinese
+        Video Platform),{" "}
+        <a href="https://yang-xijie.github.io/">Chinese Personal Homepage</a>
+      </p>
+
+      <h2>Others</h2>
+
+      <p>
+        Honors: Honor for Graduates – Excellence in Academic Innovation (2025,
+        ZJU) , Outstanding Graduate Leader Award (2024, ZJU), Award of Honor for
+        Graduate (2024, ZJU), Outstanding Scholarship for Scientific and
+        Technological Innovation (2021, THU), Outstanding Award for Social
+        Practice (2020, THU)
+      </p>
+      <p>
+        Programming: <a href="https://www.python.org">Python</a>,{" "}
+        <a href="https://pytorch.org">PyTorch</a>,{" "}
+        <a href="https://developer.nvidia.com/cuda">CUDA</a>,{" "}
+        <a href="https://developer.apple.com/metal/">Metal</a>; App dev:{" "}
+        <a href="https://developer.apple.com/swift/">Swift</a>,{" "}
+        <a href="https://developer.apple.com/swiftui/">SwiftUI</a>; Web dev:{" "}
+        <a href="https://react.dev">React</a>; Tools:{" "}
+        <a href="https://www.sketch.com">Sketch</a>,{" "}
+        <a href="https://www.apple.com/in/keynote/">Keynote</a>,{" "}
+        <a href="https://www.apple.com/final-cut-pro/">FCP</a>
+      </p>
+      <p>
+        Interesting Projects:{" "}
+        <a href="https://space.bilibili.com/24502827/lists/139476">
+          C++ on macOS Tutorial
+        </a>
+        , <a href="https://thu-mobile-dev.github.io/book/">Flutter Tutorial</a>,{" "}
+        <a href="https://github.com/Racoon-Book/Racoon-Account-Book">
+          RacoonAccountBook
+        </a>{" "}
+        (Second Prize in 2021 Mobile Application Innovation Contest),{" "}
+        <a href="https://www.bilibili.com/video/BV1Jd4y1g7eX/">Audioc</a>{" "}
+        (Second Prize in 2022 Mobile Application Innovation Contest),{" "}
+        <a href="https://github.com/Ex-Studio/ExSticky">ExSticky</a>
+      </p>
+
+      <p>Languages: native Chinese, fluent English, beginner Japanese</p>
+
+      <p>
+        Interests: <a href="https://developer.apple.com/macos/">macOS</a>,{" "}
+        <a href="https://yang-xijie.github.io/2/Anime/anime-rating-extraordinary/">
+          Anime
+        </a>
+        , <a href="https://midiplus.com/html/Xpro%20II.html">Keyboard</a>
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
